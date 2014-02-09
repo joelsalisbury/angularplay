1536cffaed8fa4b09df276e26229cd5be07b2c28
Dave Chanko <david.chanko@gmail.com>
2014-02-09 12:37:53 -0500

    Trying again to remove extra loads.

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -4,13 +4,6 @@
 
 (load (merge-pathnames "scansion.asd" *build-dir*))
 
-(push (merge-pathnames "dep/dpc-cl/" *build-dir*) asdf:*central-registry*)
-(ql:quickload :dpc-cl)
-(push (merge-pathnames "dep/web/" *build-dir*) asdf:*central-registry*)
-(ql:quickload :web)
-(push (merge-pathnames "dep/testing/" *build-dir*) asdf:*central-registry*)
-(ql:quickload :testing)
-
 (ql:quickload :scansion)
 
 ;;; Redefine / extend heroku-toplevel here if necessary.
