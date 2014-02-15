26eacd94897dde5d352e407ee75cb08d3fb0fe4f
Dave Chanko <david.chanko@gmail.com>
2014-02-15 18:57:44 -0500

    Fixing typos and cleaning up data file.

--- src/scansion.lisp
+++ src/scansion.lisp
@@ -31,21 +31,98 @@
 (defun process-line (text syllables lengths)
   (print text)
   (print syllables)
-  (print lengths))
+  (print lengths)
+  (print (coerce (cdr text) 'list))
+  (process-syllables 1 (coerce (cadr text) 'list) syllables lengths 0 0))
+
+(defun process-syllables (line-id text syllables lengths pos index)
+  (let ((sylb (coerce (car syllables) 'list)))
+    (multiple-value-bind (start len) (get-syllable-range text sylb index)
+      ;(syllable-create :line-id line-id
+		       ;:position pos
+		       ;:start (+ index start)
+		       ;:char-cnt len
+		       ;:length (car lengths)
+		       ;:text (car syllables))
+      (print (format nil "Line: ~a; Pos: ~a; Range: ~a for ~A; Text: ~a (~a)"
+		     line-id pos start len (car syllables) (car lengths)))
+
+      (when (cdr syllables)
+	(print (format nil "Remainder: ~a; Syllables: ~a; Lengths: ~a; Pos: ~a; Index: ~a" (nthcdr (+ (- start index) len) text)
+			   (cdr syllables) 
+			   (cdr lengths)
+			   (1+ pos)
+			   (+ start len)))
+	(process-syllables line-id
+			   (nthcdr (+ (- start index) len) text)
+			   (cdr syllables) 
+			   (cdr lengths)
+			   (1+ pos)
+			   (+ start len))))))
+    
+(defun get-syllable-range (text sylb &optional (start 0))
+  (multiple-value-bind (fresh-text offset) (eat-space text)
+    (let ((start (+ start offset))
+	  (length (determine-syllable-length fresh-text sylb)))
+      (values start length))))
+	 
+(define-condition bad-syllable (condition) ())     
+(defun bad-syllable-handler (condition)
+  (declare (ignore condition))
+  (format t "Bad-syllable!"))
+(handler-bind ((bad-syllable #'bad-syllable-handler)))
+  
+(defun determine-syllable-length (text sylb &optional (len 0))
+  (multiple-value-bind (match text-cnt sylb-cnt)
+      (match-text-sylb text sylb)
+    (if match
+	(if (nthcdr sylb-cnt sylb)
+	    (determine-syllable-length (nthcdr text-cnt text)
+				       (nthcdr sylb-cnt sylb) (+ len text-cnt))
+	    (+ len text-cnt))
+	(if (equal #\- (car sylb))
+	    (determine-syllable-length text (cdr sylb) len)
+	    (error 'bad-syllable)))))
+	
+
+(defun text-sylb-char-match (text sylb)
+  "Letters match if they are equal or the text is a space and the sylb is a -."
+  (or (equal (char-upcase text) (char-upcase sylb))
+	  (and (equal #\Space text) (equal #\- sylb))))
+
+(defun match-text-sylb (text sylb)
+  (let ((text-char  (car text))
+	(sylb-char (car sylb)))
+    (cond
+      ((equal (char-upcase text-char) (char-upcase sylb-char))
+       (values t 1 1))
+      ((or (and (equal #\Space text-char) (equal #\- sylb-char))
+	   (and (equal #\, text-char) (equal #\- sylb-char)))
+       (values t
+	       (multiple-value-bind (_ num) (eat-space text)
+		 (declare (ignore _)) num)
+	       1)))))
+   
+(defun eat-space (text)
+  "Return removing non-alpha-numeric chars at beginning and # chars removed."
+  (do ((i 0 (1+ i)))
+      ((cl-ppcre:scan "[A-Za-z0-9]" (string (nth i text)))
+       (values (nthcdr i text) i))))
+  
 
 (defun pre-process-text-line (line)
-  (let* ((parts (split-string line #\;))
+  (let* ((parts (split-string line #\#))
 	 (line-num (parse-integer (nth 1 parts)))
 	 (text (nth 3 parts)))
     (list line-num text)))
 
 (defun pre-process-syllable-line (line)
-  (let ((syllables (cdr (split-string line #\;))))
+  (let ((syllables (cdr (split-string line #\#))))
     (loop for syllable in syllables
        while (not (equal syllable "")) collect syllable)))
 
 (defun pre-process-length-line (line)
-  (let ((lengths (cdr (split-string line #\;))))
+  (let ((lengths (cdr (split-string line #\#))))
     (loop for length in lengths
        while (not (equal length "")) collect
 	 (cond 
