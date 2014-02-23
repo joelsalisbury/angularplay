31add33385606efd4e48d3405c71b15714adc3f1
Dave Chanko <david.chanko@gmail.com>
2014-02-23 12:18:08 -0500

    Try removing some dep loading & heroku trigger.

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -4,9 +4,9 @@
 
 (load (merge-pathnames "scansion.asd" *build-dir*))
 
-(push (merge-pathnames "dep/dpc-cl/" *build-dir*) asdf:*central-registry*)
-(push (merge-pathnames "dep/web/" *build-dir*) asdf:*central-registry*)
-(push (merge-pathnames "dep/testing/" *build-dir*) asdf:*central-registry*)
+;(push (merge-pathnames "dep/dpc-cl/" *build-dir*) asdf:*central-registry*)
+;(push (merge-pathnames "dep/web/" *build-dir*) asdf:*central-registry*)
+;(push (merge-pathnames "dep/testing/" *build-dir*) asdf:*central-registry*)
 
 (ql:quickload :scansion)
 
